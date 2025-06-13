1a6599739d0f1189e6591cbc842592ae2fe6d602
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-06-13 16:25:55 -0300

    SEO, meta tags, favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,32 @@ const gloock = Gloock({
 });
 
 export const metadata: Metadata = {
-  title: "Juliadev",
-  description: "Juliadev next.js",
+  title: "Julia Bacchi | Front-end Developer",
+  description:
+    "Portfólio de Julia Bacchi - Desenvolvedora front-end com foco em React, Next.js e TailwindCSS.",
+  keywords: [
+    "desenvolvimento front-end",
+    "React",
+    "Next.js",
+    "TailwindCSS",
+    "portfólio Julia Bacchi",
+  ],
+  openGraph: {
+    title: "Julia Bacchi | Front-end Developer",
+    description:
+      "Portfólio de Julia Bacchi - Desenvolvedora front-end com foco em React, Next.js e TailwindCSS.",
+    url: "https://juliadev.vercel.app",
+    type: "website",
+    siteName: "Portfólio Julia Bacchi",
+    images: "/og-image.jpg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Julia Bacchi | Front-end Developer",
+    description:
+      "Portfólio de Julia Bacchi - Desenvolvedora front-end com foco em React, Next.js e TailwindCSS.",
+    images: ["/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +51,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className={`${gloock.variable} ${montserrat.variable}`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Título no iOS */}
+        <meta name="apple-mobile-web-app-title" content="Julia Dev" />
+        <meta name="application-name" content="Julia Dev" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+
+        {/* Favicons */}
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+
+        {/* PWA icons - Android */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/favicon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicon/favicon-512x512.png"
+        />
+      </head>
       <body>
         <Navbar />
         {children}
