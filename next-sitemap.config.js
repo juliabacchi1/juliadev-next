b2f48ef7023a40de7619c0ad330e75f77d012e96
Júlia Bacchi <134342345+juliabacchi1@users.noreply.github.com>
2025-07-18 13:45:46 -0300

    Config sitemap and vercels direction https

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://juliabacchi.com",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 7000,
+  trailingSlash: false,
+};
